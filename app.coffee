4faa227ac0e4f494d8998cef81af48c4d4d8ec93
lei <lei@lei-VirtualBox.(none)>
2012-11-10 21:52:39 -0800

    video changes

--- app.coffee
+++ app.coffee
@@ -3,7 +3,7 @@ express = require 'express'
 app = express()
 http = require 'http'
 httpserver = http.createServer(app)
-httpserver.listen(6666)
+httpserver.listen(3000)
 nowjs = require 'now'
 everyone = nowjs.initialize(httpserver)
 
@@ -35,3 +35,10 @@ everyone.now.getSubAtTime = getSubAtTime
 
 everyone.now.sendWordsToServer = (words) ->
   everyone.now.singerReceivesWords(words)
+
+everyone.now.getToken = (callback) ->
+  #key = '21484962';    // Replace with your API key  
+  #secret = '1d3cac96fdbd675a881c6f59b1774408c75d903d';  // Replace with your API secret  
+  #opentok = new OpenTok.OpenTokSDK(key, secret);
+  token = 'T1==cGFydG5lcl9pZD0yMTQ4NDk2MiZzaWc9MDZlYzRlOTYwYzE4NjIwY2YxNzE5OGM5MWE4ODMxYjZhZWRlZjkzNTpzZXNzaW9uX2lkPTJfTVg0eU1UUTRORGsyTW41LVUyRjBJRTV2ZGlBeE1DQXhOem96TURvd01TQlFVMVFnTWpBeE1uNHdMalk1TkRjMk9UUi0mY3JlYXRlX3RpbWU9MTM1MjYwNzExOSZleHBpcmVfdGltZT0xMzU1MTk5MTE5JnJvbGU9cHVibGlzaGVyJmNvbm5lY3Rpb25fZGF0YT0mbm9uY2U9NTI4MjAy' # Replace with a generated token. See http://static.opentok.com/opentok/api/tools/generator
+  callback(token)
--- tokboxvideoconfiguration.js
+++ tokboxvideoconfiguration.js
@@ -1,20 +1,30 @@
 var apiKey = "21484962"; // Replace with your API key. See https://dashboard.tokbox.com
 var sessionId = '2_MX4yMTQ4NDk2Mn5-U2F0IE5vdiAxMCAxNzozMDowMSBQU1QgMjAxMn4wLjY5NDc2OTR-'; // Replace with your own session ID. See http://static.opentok.com/opentok/api/tools/generator 
-var token = 'T1==cGFydG5lcl9pZD0yMTQ4NDk2MiZzaWc9ZTAxMGY3YjZlOTBiYTQ0YjVmNzJhNzJhNGFkM2FhOWFlMTNiZTliYTpzZXNzaW9uX2lkPTJfTVg0eU1UUTRORGsyTW41LVUyRjBJRTV2ZGlBeE1DQXhOem96TURvd01TQlFVMVFnTWpBeE1uNHdMalk1TkRjMk9UUi0mY3JlYXRlX3RpbWU9MTM1MjU5NzM5MCZleHBpcmVfdGltZT0xMzUyNjgzNzkwJnJvbGU9cHVibGlzaGVyJmNvbm5lY3Rpb25fZGF0YT0mbm9uY2U9MjQ5MzY4'; // Replace with a generated token. See http://static.opentok.com/opentok/api/tools/generator
+//var token = 
+//'T1==cGFydG5lcl9pZD0yMTQ4NDk2MiZzaWc9ZTAxMGY3YjZlOTBiYTQ0YjVmNzJhNzJhNGFkM2FhOWFlMTNiZTliYTpzZXNzaW9uX2lkPTJfTVg0eU1UUTRORGsyTW41LVUyRjBJRTV2ZGlBeE1DQXhOem96TURvd01TQlFVMVFnTWpBeE1uNHdMalk1TkRjMk9UUi0mY3JlYXRlX3RpbWU9MTM1MjU5NzM5MCZleHBpcmVfdGltZT0xMzUyNjgzNzkwJnJvbGU9cHVibGlzaGVyJmNvbm5lY3Rpb25fZGF0YT0mbm9uY2U9MjQ5MzY4'; // Replace with a generated token. See http://static.opentok.com/opentok/api/tools/generator
 
 TB.setLogLevel(TB.DEBUG); // Set this for helpful debugging messages in console
 
 var session = TB.initSession(sessionId);
 session.addEventListener('sessionConnected', sessionConnectedHandler);
 session.addEventListener('streamCreated', streamCreateHandler);
-session.connect(apiKey, token);
+//session.connect(apiKey, token);
+now.ready(function() {
+  now.getToken(function(token) {
+    $('#replaceMe').append('<span id="myPublishers"><span id="fillStuff"></span></span><span id="myStreams"></span>');
+    $('#myPublishers')[0].setAttribute('style','padding-left:10px');
+    $("#myStreams")[0].setAttribute('style', 'padding-left:10px');
+    session.connect(apiKey, token);
+
+  })
+})
 
 var publisher;
 
 function sessionConnectedHandler(event) {
-	//lert('Hello world. I am connected to OpenTok :).');
+	//alert('Hello world. I am connected to OpenTok :).');
 	var publishProps = {height:240, width:320};
-	publisher = TB.initPublisher(apiKey, 'myPublisherDiv', publishProps);
+	publisher = TB.initPublisher(apiKey, 'fillStuff', publishProps);
 	session.publish(publisher);
 	subscribeToStreams(event.streams);
 }
@@ -24,18 +34,18 @@ function streamCreateHandler(event) {
 }
 
 function subscribeToStreams(streams) {
-	console.log('blah:'+ streams.length);
+	//console.log('blah:'+ streams.length);
 	for (var i = 0; i <streams.length; i++) {
 		if (streams[i].connection.connectionId == session.connection.connectionId)
 		{
 			return;
 		}
 
-		var div = document.createElement('div');
+		var div = document.createElement('span');
 		div.setAttribute('id', 'stream' + streams[i].streamId);
-		$("#myStreamsDiv").append(div);
-
+		div.setAttribute('style', 'padding-left:10px');
+		$("#myStreams").append(div);
 		var subscribeProps = {height:240, width:320};
-		session.subscribe(streams[i], div.id);
+		session.subscribe(streams[i], div.id, subscribeProps);
 	}
 }
